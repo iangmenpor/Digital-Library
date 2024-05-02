93afe48368a23c18583bcfb3b3c38bef0b13ae12
Ian Gabriel Mendoza Portillo <145766951+iangmenpor@users.noreply.github.com>
2024-05-02 20:40:14 +0200

    feat: Consultar a un Usuario.
    - Creación de caso de uso para consultar y devolver información de un Usuario dado de alta en la biblioteca virtual (GetUseCase), y presentación en menu.

--- src/main/java/com/iesam/digitalLibrary/features/user/data/local/DataSourceRepository.java
+++ src/main/java/com/iesam/digitalLibrary/features/user/data/local/DataSourceRepository.java
@@ -5,4 +5,6 @@ import com.iesam.digitalLibrary.features.user.domain.User;
 public interface DataSourceRepository {
 
     void save (User model);
+
+    User findById(Integer id);
 }
--- src/main/java/com/iesam/digitalLibrary/features/user/domain/UserRepository.java
+++ src/main/java/com/iesam/digitalLibrary/features/user/domain/UserRepository.java
@@ -3,4 +3,5 @@ package com.iesam.digitalLibrary.features.user.domain;
 public interface UserRepository {
 
     void saveUser(User model);
+    User getUser(Integer id);
 }
