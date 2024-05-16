c22a5479242fce5d4f8f6025f1c8547147785b2c
Ian Gabriel Mendoza Portillo <145766951+iangmenpor@users.noreply.github.com>
2024-05-16 12:36:07 +0200

    Test: recuperar préstamo.
    - Se ha modelado el test para el caso de uso get de préstamos (loan).

---
+++ src/test/java/com/iesam/digitalLibrary/features/loan/domain/GetLoanUseCaseTest.java
@@ -0,0 +1,64 @@
+package com.iesam.digitalLibrary.features.loan.domain;
+
+import com.iesam.digitalLibrary.features.digitalResources.domain.DigitalResource;
+import com.iesam.digitalLibrary.features.digitalResources.domain.EBook;
+import com.iesam.digitalLibrary.features.user.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class GetLoanUseCaseTest {
+
+    @Mock
+    private LoanRepository loanRepository;
+    private GetLoanUseCase getLoanUseCase;
+
+    @BeforeEach
+    void setUp() {
+        getLoanUseCase = new GetLoanUseCase(loanRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        getLoanUseCase = null;
+    }
+
+    @Test
+    public void  cuandoSeBuscaIdDeUnPrestamoExistenteDebeDevolverPrestamo() {
+        //Given
+        ArrayList<DigitalResource> resources = new ArrayList<>();
+        resources.add(new EBook(1, "TestISBN1", "TestTitle1", "TestAuthor1", "TestNumPages1", "TestFormat1"));
+        resources.add(new EBook(2, "TestISBN2", "TestTitle2", "TestAuthor2", "TestNumPages2", "TestFormat2"));
+        resources.add(new EBook(3, "TestISBN3", "TestTitle3", "TestAuthor3", "TestNumPages3", "TestFormat3"));
+        User user = new User(1,"NameTest","SurnameTest","DNITest","EmailTest");
+
+        Loan expectedLoan = new Loan(1,user, resources, null);
+        Mockito.when(loanRepository.getLoan(1)).thenReturn(expectedLoan);
+
+        //When
+        Loan actualLoan = getLoanUseCase.execute(1);
+
+        //Then
+        assertEquals(expectedLoan, actualLoan);
+    }
+
+    @Test
+    public void cuandoSeBuscaIdDePrestamoNoExistenteDevolverNull(){
+        //Give
+        Mockito.when(loanRepository.getLoan(-999)).thenReturn(null);
+
+        //When
+        Loan actualLoan = getLoanUseCase.execute(-999);
+
+        //Then
+        assertNull(actualLoan);
+    }
+}
